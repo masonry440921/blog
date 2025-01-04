3072068b19327230400b45c8320c93c54de840d9
masonry440921 <free-masonry@hotmail.com>
2025-01-05 01:19:23 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: 'https://masonry440921.github.io',
+  base: 'blog',
   trailingSlash: "always",
   integrations: [
     tailwind(
