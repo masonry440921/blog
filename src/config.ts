20abe4c9da8b743a9b36cc3222144ef1f7a53749
masonry440921 <free-masonry@hotmail.com>
2025-01-05 01:57:22 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -52,8 +52,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/mason.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: 'mason',
+  bio: '',
   links: [
     {
       name: 'Twitter',
