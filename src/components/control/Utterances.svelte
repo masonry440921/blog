e7f76bd96e241bb603a33ca59d15047fb15990c4
masonry440921 <free-masonry@hotmail.com>
2025-01-16 20:49:49 +0800

    Create Utterances.svelte

---
+++ src/components/control/Utterances.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  export let repo: string;
+  export let issueTerm: string = 'url';
+  export let label: string = 'post comments';
+  export let theme: string = 'github-light';
+
+  onMount(() => {
+    const script = document.createElement('script');
+    const container = document.getElementById('utterances-container');
+
+    Object.entries({
+      src: 'https://utteranc.es/client.js',
+      repo,
+      'issue-term': issueTerm,
+      label,
+      theme,
+      crossorigin: 'anonymous',
+    }).forEach(([key, value]) => {
+      script.setAttribute(key, value as string);
+    });
+
+    container?.appendChild(script);
+  });
+</script>
+
+<div id="utterances-container"></div>
